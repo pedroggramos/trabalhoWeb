857777bbd6f85ffe3158a76837e05a630d241f1e
pedroggramos <pedrogabrielguimaraesramos178@gmail.com>
2024-11-07 22:37:18 -0300

    seja no mar

--- script.js
+++ script.js
@@ -12,4 +12,35 @@ function showLandingPage() {
     }, 500); // Espera o tempo da transição da página inicial
 }
 
-// dropdown
+// login
+
+function openNav() {  
+    document.getElementById("overlay").style.width = "15%";  
+}  
+
+function closeNav() {  
+    document.getElementById("overlay").style.width = "0%";  
+}  
+
+// carrossel
+
+const cards = document.querySelectorAll('.card');  
+let currentIndex = 0;  
+
+function updateCarousel() {  
+    const offset = -currentIndex * 220; // 220px é a largura do card + margin  
+    cards.forEach(card => {  
+        card.style.transform = `translateX(${offset}px)`;  
+    });  
+}  
+
+document.getElementById('next').addEventListener('click', () => {  
+    currentIndex = (currentIndex + 1) % cards.length;  
+    updateCarousel();  
+});  
+
+document.getElementById('prev').addEventListener('click', () => {  
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;  
+    updateCarousel();  
+});
+
